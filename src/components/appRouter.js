02a09bb35b2a095f60da894456cf2d878f0b61e8
German Carrillo <german.carillo@aurea.com>
2019-03-16 14:58:47 -0500

    Changes in router and package json

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Title } from './title';
-import { Task } from './task';
-import { TaskList } from './taskList';
+import Title from './title';
+import Task from './task';
+import TaskList from './taskList';
 
 export default class appRouter extends Component {
   render() {
