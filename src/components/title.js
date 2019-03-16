67eca39d642ba5216c19f54c1737bef7b33e59bd
Antonio Insuasti <antonio@insuasti.ec>
2019-03-16 15:23:53 -0500

    Add antd button

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Button from 'antd/lib/button';
+import '../App.css';
 
 export default class title extends Component {
   render() {
     return (
-      <div>
-        <h1>Todo list</h1>
+      <div className="App">
+        <h1>Todo list <span><Button className={{ float: "right" }} type="primary">+</Button></span></h1>     
       </div>
     )
   }
