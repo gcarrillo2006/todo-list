4cae35a94132074470bb79ae8a1d3f2a75596fd0
German Carrillo <german.carillo@aurea.com>
2019-03-16 14:51:22 -0500

    Router completo

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Title } from './title';
+import { Task } from './task';
+import { TaskList } from './taskList';
 
 export default class appRouter extends Component {
   render() {
     return (
       <div>
             <Router>
-                <React.Fragment>
+                <Switch>
                     <Route path="/" component={Title} />
-                </React.Fragment>
+                    <Route path="/task" component={Task} />
+                    <Route path="/task-list" component={TaskList} />
+                </Switch>
             </Router>
       </div>
     )
