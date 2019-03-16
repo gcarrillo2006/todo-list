75061ecd9e777b1bfbfe01c28545047f01731be5
German Carrillo <german.carillo@aurea.com>
2019-03-16 14:45:04 -0500

    Upload router

---
+++ src/components/appRouter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+export default class appRouter extends Component {
+  render() {
+    return (
+      <div>
+            <Router>
+                <React.Fragment>
+                
+                </React.Fragment>
+            </Router>
+      </div>
+    )
+  }
+}
